7ded3543671ee933587d01a78dfc2ba299953cb2
Angelo Guerrero <amcgdev@gmail.com>
2020-02-07 11:46:05 +0400

    - Added box shadow to various components - Fixed entry animation for movements and charities list component

--- src/components/move-and-char-list/move-and-char-list.js
+++ src/components/move-and-char-list/move-and-char-list.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from './move-and-char-list.module.css';
 import { motion } from 'framer-motion';
-import { lefty } from '../motion-variants/left-content-variant'; 
 
 export const MoveAndCharList = () => {
     //TODO: FILL THIS UP WITH STUFF
     return (
-      <motion.div className={styles.listContainer} initial="initial" animate="animate" transition={{delay: 1.25}} variant={lefty}>
+      <motion.div className={styles.listContainer} initial={{opacity: 0, x: '-100vh'}} animate={{opacity:1, x:'0vh'}} transition={{delay: 1.25}}  >
         <h1>Test</h1>
          <ul className={styles.charMoveList}>
              <li className={styles.listItem}>Lorem</li>
